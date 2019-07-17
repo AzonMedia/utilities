3f9735871d6d7184834be1ab7fa757d8c751d8a0
Vesko Kenashkov <vesko@azonmedia.com>
2019-07-17 18:06:22 +0300

    wip

--- src/ArrayUtil.php
+++ src/ArrayUtil.php
@@ -7,4 +7,23 @@ namespace Azonmedia\Utilities;
 abstract class ArrayUtil
 {
 
+    public static function validate_array(array $data_array, array $reference_array) : bool
+    {
+        $ret = TRUE;
+        foreach ($data_array as $key=>$value) {
+            if (!array_key_exists($key, $reference_array)) {
+                $ret = FALSE;
+                break;
+            } elseif (is_array($value)) {
+                $ret = self::validate_array($value, $reference_array[$key]);
+                if (!$ret) {
+                    break;
+                }
+            } elseif (!TypesUtil::validate_type($value, $reference_array[$key])) {
+                $ret = FALSE;
+                break;
+            }
+        }
+        return $ret;
+    }
 }
---
+++ src/TypesUtil.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace Azonmedia\Utilities;
+
+
+abstract class TypesUtil
+{
+
+    public const TYPES_SYNONYMS = [
+        ['int', 'integer'],
+        ['float', 'double'],
+        ['bool', 'boolean'],
+        ['', 'mixed'],//pseudotype
+    ];
+
+    public static function validate_type( /* mixed */ $value, string $expected_type) : bool
+    {
+        $value_type = gettype($value);
+
+        if ($expected_type==='' || $expected_type==='mixed') {
+            return TRUE;
+        }
+
+        if (strtolower($value_type) === strtolower($expected_type)) {
+            return TRUE;
+        }
+
+        foreach (self::TYPES_SYNONYMS as $synonyms) {
+            if (in_array( strtolower($value_type), array_map('strtolower', $synonyms)) && in_array( strtolower($expected_type), array_map('strtolower',$synonyms) ) ) {
+                return TRUE;
+            }
+        }
+
+        return FALSE;
+    }
+}
