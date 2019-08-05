53c40dfa129d99a9fbee00b61d70877ef859c9d6
Ronald Hristov <ronald@azonmedia.com>
2019-08-05 13:10:56 +0300

    Added: Array to string function

--- src/ArrayUtil.php
+++ src/ArrayUtil.php
@@ -6,7 +6,11 @@ namespace Azonmedia\Utilities;
 
 abstract class ArrayUtil
 {
-
+    /**
+     * @param array $data_array
+     * @param array $reference_array
+     * @return bool
+     */
     public static function validate_array(array $data_array, array $reference_array) : bool
     {
         $ret = TRUE;
@@ -25,5 +29,27 @@ abstract class ArrayUtil
             }
         }
         return $ret;
+    }
+
+    /**
+     * The array is expected to have only values that can be cast to strings.
+     * To be used for hashing arrays
+     * @param array $array Multidimensional array
+     * @return string The concatenated string of keys and values
+     */
+    public static function array_as_string(array $array)
+    {
+        $str = '';//concatenated keys and values recuresively
+        $func = function (array $array) use (&$str, &$func) {
+            foreach ($array as $key => $value) {
+                if (is_array($value)) {
+                    $func($value);
+                } else {
+                    $str .= $key . $value;
+                }
+            }
+        };
+        $func($array);
+        return $str;
     }
 }
