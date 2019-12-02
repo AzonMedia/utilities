dab96200949a8fe22a3830a9ee7cbba3c7fdd4f1
Vesko Kenashkov <vesko@azonmedia.com>
2019-12-02 17:13:55 +0200

    added debug utilities

---
+++ src/DebugUtil.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Azonmedia\Utilities;
+
+
+class DebugUtil
+{
+    public static function dump_code_with_lines(string $code) : string
+    {
+        $ret = '';
+        $code = explode(PHP_EOL, $code);
+        foreach ($code as $line_num => $code_line) {
+            $ret .= '#'.$line_num.str_repeat(' ',4).$code_line.PHP_EOL;
+        }
+        return $ret;
+    }
+}
