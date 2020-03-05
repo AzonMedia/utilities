e78fbfab8b9c65a4a1ecf3e7e555b3eefdeada13
Vesko Kenashkov <vesko@azonmedia.com>
2020-03-05 17:41:07 +0200

    added String Utils

---
+++ src/StringUtil.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Azonmedia\Utilities;
+
+
+class StringUtil
+{
+
+    public static function starts_with(string $haystack, string $needle) : bool
+    {
+        $length = strlen($needle);
+        return (substr($haystack, 0, $length) === $needle);
+    }
+
+    public static function ends_with(string $haystack, string $needle) : bool
+    {
+        $length = strlen($needle);
+        if ($length === 0) {
+            return TRUE;
+        }
+
+        return (substr($haystack, -$length) === $needle);
+    }
+}
